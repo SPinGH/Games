a20def494ce0042c449caa6e72aae5625d94c7bb
SPinGH <salamatovpaul@yandex.ru>
2021-08-16 18:11:52 +0300

    Added saving current theme in local storage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ export default class App {
         this.curTheme = null;
         this.rootElement = element;
         if (Themes.length > 0) {
+            if (localStorage.currentTheme) {
+                let theme = Themes.find(({ name }) => name === localStorage.currentTheme);
+                if (theme) {
+                    this.ChangeTheme(theme);
+                    return;
+                }
+            }
             this.ChangeTheme(Themes[0])
         }
     }
 
-    ChangeTheme(theme) {
+    ChangeTheme = (theme) => {
         if (theme && 'getModule' in theme) {
             theme.getModule().then(module => {
-                this.curTheme = new module.default(this.rootElement, this.ChangeTheme.bind(this));
+                this.curTheme = new module.default(this.rootElement, this.ChangeTheme);
                 document.title = theme.name;
+                localStorage.setItem('currentTheme', theme.name);
             }).catch(error => {
                 console.error(error);
             })
